839c0c8d833cda44563cc75efe995b28457731ff
SaqibWani272 <saqibwani273@gmail.com>
2022-12-17 04:39:02 +0530

    upload cancel feature added

--- lib/tabs/upload_video_tab/models/upload_data.dart
+++ lib/tabs/upload_video_tab/models/upload_data.dart
@@ -1,19 +1,25 @@
 //import 'dart:html';
+import 'dart:async';
 import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
-import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
 
 class UploadData extends ChangeNotifier {
   UploadData(BuildContext context);
   var isVideoSelected = false;
   var isImageSelected = false;
   var isUploading = false;
+  var isPaused = false;
   late PlatformFile selectedVideo;
   late String selectedImagePath;
-  var progress = 0.0;
-  // late String fileName;
+  late UploadTask uploadTask;
+  int progress = 0;
+  var isUploadSuccessfull = false;
+  var isUploadCancelled = false;
+  // function to select video using filePicker
   Future selectVideo() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles(
       type: FileType.video,
@@ -32,6 +38,7 @@ class UploadData extends ChangeNotifier {
     }
   }
 
+// function to select image using filePicker()
   Future selectImage() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles(
       type: FileType.image,
@@ -41,14 +48,43 @@ class UploadData extends ChangeNotifier {
     notifyListeners();
   }
 
-  cancelUploading() {
-    //...code to cancel video uplaod
+//function to cancel ongoing upload
+  cancelUploading(BuildContext contxt) {
+    print('cancel uploading called');
+
+    uploadTask.cancel();
+    isUploadCancelled = true;
+    ScaffoldMessenger.of(contxt).showSnackBar(
+      SnackBar(
+        content: Text(
+          'Uploading Cancelled !!!',
+        ),
+      ),
+    );
+
+    notifyListeners();
   }
+
+  pauseUploading() {
+    uploadTask.pause();
+    isPaused = true;
+    notifyListeners();
+  }
+
+  resumeUploading() {
+    uploadTask.resume();
+    isPaused = false;
+    notifyListeners();
+  }
+
+// function to finally upload video to firebase
+// using selected video, selected thumbnail image and other details
   uploadVideo({
     required String title,
     required String date,
     required String speaker,
     required String description,
+    required BuildContext context,
   }) async {
     // firebase path to upload videos at
     String filePath = 'uploads/${selectedVideo.name}';
@@ -56,40 +92,63 @@ class UploadData extends ChangeNotifier {
 
     try {
       final ref = FirebaseStorage.instance.ref(filePath);
-      await ref.putFile(videoFile).snapshotEvents.listen((taskSnapshot) {
-        switch (taskSnapshot.state) {
-          case TaskState.running:
-            progress =
-                100 * taskSnapshot.bytesTransferred / taskSnapshot.totalBytes;
-            notifyListeners();
-            break;
-          case TaskState.paused:
-            // ...
-            break;
-          case TaskState.success:
-            final metaData = SettableMetadata(customMetadata: {
-              "title": title,
-              "speaker": speaker,
-              "description": description,
-              "date": date,
-            });
-            print(date);
-            ref.updateMetadata(metaData);
-            isVideoSelected = !isVideoSelected;
-            isUploading = !isUploading;
-            notifyListeners();
-            break;
-          case TaskState.canceled:
-            // ...
-            break;
-          case TaskState.error:
-            // ...
-            break;
-        }
-      });
+      uploadTask = ref.putFile(videoFile);
+      // to listen to ongoing progress while uploading
+      uploadTask.snapshotEvents.listen(
+        (taskSnapshot) {
+          switch (taskSnapshot.state) {
+            // case 1:
+            case TaskState.running:
+              progress = (100 *
+                      taskSnapshot.bytesTransferred /
+                      taskSnapshot.totalBytes)
+                  .floor();
+
+              notifyListeners();
+              break;
+            //case 2:
+            case TaskState.paused:
+              //this works fine
+              print('taskState paused called');
+              break;
+            //case 3:
+            case TaskState.success:
+              final metaData = SettableMetadata(customMetadata: {
+                "title": title,
+                "speaker": speaker,
+                "description": description,
+                "date": date,
+              });
+              print(date);
+              ref.updateMetadata(metaData);
+              isUploadSuccessfull = true;
+              print('success executed');
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(
+                  content: Text(
+                    'video Uploaded Successfully !',
+                  ),
+                ),
+              );
+              notifyListeners();
+              break;
+// case 4:
+            case TaskState.canceled:
+              // not getting executed...
+              print('task state cancelled called');
+              break;
+//case 5:
+            case TaskState.error:
+              print('taskstate.error executed');
+              // ...
+              break;
+          }
+        },
+      );
     } catch (e) {
-      // ...
-      print(e.toString());
+      //
+      print(
+          'error type = ${e.runtimeType}\nand error.toString=${e.toString()}');
     }
     isUploading = true;
     notifyListeners();
--- lib/tabs/upload_video_tab/upload_main_screen.dart
+++ lib/tabs/upload_video_tab/upload_main_screen.dart
@@ -14,140 +14,174 @@ class UploadTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // final selectedVideoName = upload.fileName;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Info tab'),
       ),
       body: Center(
-        child: upload.isVideoSelected
-            ?
-            // this widget will be displayed after
-            //video is selected by user
-            Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Form(
-                      key: _formKey,
-                      child: SingleChildScrollView(
-                        child: Column(
-                          children: [
-                            // Image.file(
-                            //   new File(upload.videoPath),
-                            //   scale: 1.0,
-                            //   repeat: ImageRepeat.noRepeat,
-                            // ),
-                            Text(
-                                'Selected video :${upload.selectedVideo.name}'),
-                            if (upload.isImageSelected)
-                              Image.file(File(upload.selectedImagePath)),
-                            TextButton.icon(
-                              onPressed: upload.selectImage,
-                              icon: Icon(upload.isImageSelected
-                                  ? Icons.image_rounded
-                                  : Icons.attach_file_outlined),
-                              label: upload.isImageSelected
-                                  ? Text('Thumbnail Image')
-                                  : Text("select a thumbnail image    *"),
-                            ),
-                            SizedBox(height: 10),
-                            TextFormField(
-                              controller: videoTitle,
-                              decoration: InputDecoration(
-                                hintText: 'Enter the video Title *',
+          child: !(upload.isVideoSelected) ||
+                  upload.isUploadCancelled ||
+                  upload.isUploadSuccessfull
+              ? ElevatedButton(
+                  onPressed: upload.selectVideo,
+                  child: Text("Select a Video"),
+                )
+              :
+              // this widget will be displayed after
+              //video is selected by user
+              Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Form(
+                        key: _formKey,
+                        child: SingleChildScrollView(
+                          child: Column(
+                            children: [
+                              // Image.file(
+                              //   new File(upload.videoPath),
+                              //   scale: 1.0,
+                              //   repeat: ImageRepeat.noRepeat,
+                              // ),
+                              Text(
+                                  'Selected video :${upload.selectedVideo.name}'),
+                              if (upload.isImageSelected)
+                                Image.file(
+                                  File(upload.selectedImagePath),
+                                  width: 200,
+                                  height: 100,
+                                ),
+                              TextButton.icon(
+                                onPressed: upload.selectImage,
+                                icon: Icon(upload.isImageSelected
+                                    ? Icons.image_rounded
+                                    : Icons.attach_file_outlined),
+                                label: upload.isImageSelected
+                                    ? Text('Thumbnail Image')
+                                    : Text("select a thumbnail image    *"),
                               ),
-                              validator: ((value) {
-                                if (value == Null || value!.isEmpty)
-                                  return 'Enter a Title';
-                                else
-                                  return null;
-                              }),
-                            ),
-                            SizedBox(height: 10),
-                            TextFormField(
-                              controller: speaker,
-                              decoration: InputDecoration(
-                                  hintText: "Enter Speaker's Name    *"),
-                              validator: ((value) {
-                                if (value == Null || value!.isEmpty)
-                                  return "Enter Speaker's Name  ";
-                                else
-                                  return null;
-                              }),
-                            ),
-                            SizedBox(height: 10),
-                            TextFormField(
-                              controller: videoDate,
-                              decoration:
-                                  InputDecoration(hintText: 'Enter the Date'),
-                            ),
-                            SizedBox(height: 10),
-                            TextFormField(
-                              controller: videoDescription,
-                              decoration: InputDecoration(
-                                hintText:
-                                    'Enter the Brief Description about the video     *',
+                              SizedBox(height: 10),
+                              TextFormField(
+                                controller: videoTitle,
+                                decoration: InputDecoration(
+                                  hintText: 'Enter the video Title *',
+                                ),
+                                validator: ((value) {
+                                  if (value == Null || value!.isEmpty)
+                                    return 'Enter a Title';
+                                  else
+                                    return null;
+                                }),
                               ),
-                              validator: ((value) {
-                                if (value == Null || value!.isEmpty)
-                                  return 'Enter a Description';
-                                else
-                                  return null;
-                              }),
-                            ),
-                            SizedBox(height: 10),
-                            Row(
-                              children: [
-                                upload.isUploading
-                                    ? Row(
-                                        children: [
-                                          ElevatedButton(
-                                            child: Text('Cancel upload'),
-                                            onPressed: upload.cancelUploading,
-                                          ),
-                                          ElevatedButton(
-                                            child: Text(
-                                                'Uploading Video ${upload.progress} %'),
-                                            onPressed: upload.cancelUploading,
-                                          )
-                                        ],
-                                      )
-                                    : ElevatedButton(
-                                        child: Text('Upload'),
-                                        onPressed: () {
-                                          if (_formKey.currentState!
-                                                  .validate() &&
-                                              upload.isImageSelected) {
-                                            ScaffoldMessenger.of(context)
-                                                .showSnackBar(
-                                              SnackBar(
-                                                content:
-                                                    Text('Uploading Video...'),
-                                              ),
-                                            );
-                                            upload.uploadVideo(
-                                              title: videoTitle.text,
-                                              speaker: speaker.text,
-                                              description:
-                                                  videoDescription.text,
-                                              date: videoDate.text == ""
-                                                  ? DateTime.now().toString()
-                                                  : videoDate.text,
-                                            );
-                                          }
-                                        },
-                                      ),
-                              ],
-                            )
-                          ],
-                        ),
-                      )),
-                ),
-              )
-            : ElevatedButton(
-                onPressed: upload.selectVideo,
-                child: Text("Select a Video"),
-              ),
-      ),
+                              SizedBox(height: 10),
+                              TextFormField(
+                                controller: speaker,
+                                decoration: InputDecoration(
+                                    hintText: "Enter Speaker's Name    *"),
+                                validator: ((value) {
+                                  if (value == Null || value!.isEmpty)
+                                    return "Enter Speaker's Name  ";
+                                  else
+                                    return null;
+                                }),
+                              ),
+                              SizedBox(height: 10),
+                              TextFormField(
+                                controller: videoDate,
+                                decoration:
+                                    InputDecoration(hintText: 'Enter the Date'),
+                              ),
+                              SizedBox(height: 10),
+                              TextFormField(
+                                controller: videoDescription,
+                                decoration: InputDecoration(
+                                  hintText:
+                                      'Enter the Brief Description about the video     *',
+                                ),
+                                validator: ((value) {
+                                  if (value == Null || value!.isEmpty)
+                                    return 'Enter a Description';
+                                  else
+                                    return null;
+                                }),
+                              ),
+                              SizedBox(height: 10),
+                              Row(
+                                children: [
+                                  upload.isUploading
+                                      ? Row(
+                                          children: [
+                                            ElevatedButton(
+                                              child: Text('Cancel upload'),
+                                              onPressed: () {
+                                                upload.cancelUploading(context);
+                                              },
+                                            ),
+                                            upload.isPaused
+                                                ? ElevatedButton(
+                                                    child:
+                                                        Text('resume upload'),
+                                                    onPressed:
+                                                        upload.resumeUploading,
+                                                  )
+                                                : ElevatedButton(
+                                                    child: Text('pause upload'),
+                                                    onPressed:
+                                                        upload.pauseUploading,
+                                                  ),
+                                            ElevatedButton(
+                                              child: Text(
+                                                  'Uploading Video ${upload.progress} %'),
+                                              onPressed: () {},
+                                            )
+                                          ],
+                                        )
+                                      : ElevatedButton(
+                                          child: Text('Upload'),
+                                          onPressed: () {
+                                            if (_formKey.currentState!
+                                                    .validate() &&
+                                                upload.isImageSelected) {
+                                              ScaffoldMessenger.of(context)
+                                                  .showSnackBar(
+                                                SnackBar(
+                                                  content: Text(
+                                                      'Uploading Video...'),
+                                                ),
+                                              );
+                                              upload.uploadVideo(
+                                                title: videoTitle.text,
+                                                speaker: speaker.text,
+                                                description:
+                                                    videoDescription.text,
+                                                date: videoDate.text == ""
+                                                    ? DateTime.now().toString()
+                                                    : videoDate.text,
+                                                context: context,
+                                              );
+                                              if (upload.isUploadSuccessfull)
+                                                ScaffoldMessenger.of(context)
+                                                    .showSnackBar(
+                                                  SnackBar(
+                                                    content: Text(
+                                                      'Video Uploaded Successfully !',
+                                                    ),
+                                                  ),
+                                                );
+                                            }
+                                          },
+                                        ),
+                                ],
+                              )
+                            ],
+                          ),
+                        )),
+                  ),
+                )
+          //displayed if user didnot select any video yet
+          //or video uploaded successfully or video cancelled by user
+
+          ),
     );
   }
 }
